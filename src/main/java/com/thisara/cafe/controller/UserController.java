ad3967db8d3093788cb7cce40333238474675b30
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2026-02-27 20:17:11 +0530

    feat: add UserController for user registration and login endpoints

---
+++ src/main/java/com/thisara/cafe/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.thisara.cafe.controller;
+
+import com.thisara.cafe.dto.LoginRequest;
+import com.thisara.cafe.service.UserService;
+import com.thisara.cafe.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(userService.login(loginRequest));
+    }}
+
+
