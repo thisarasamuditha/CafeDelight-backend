91a789031593a00f261d474c347452ecf9f8de35
thisarasamuditha <thellamburavithanagethisarasam@gmail.com>
2026-02-28 23:09:57 +0530

    feat: add ItemController for managing item retrieval

---
+++ src/main/java/com/thisara/cafe/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.thisara.cafe.controller;
+
+import com.thisara.cafe.entity.Item;
+import com.thisara.cafe.service.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAllItems() {
+        return ResponseEntity.ok(itemService.getAllItems());
+    }
+}
